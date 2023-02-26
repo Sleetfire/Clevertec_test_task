5fe43de2454b459035bd9d9ffec32c5ca7bf6950
SleetFire <barkovskiy2000@gmail.com>
2023-02-26 20:53:31 +0300

    feat: add password validation class

---
+++ check_runner/src/main/java/com/barkovsky/check_runner/reflection_task/annotation/validation_class/PasswordValidation.java
@@ -0,0 +1,25 @@
+package com.barkovsky.check_runner.reflection_task.annotation.validation_class;
+
+import com.barkovsky.check_runner.reflection_task.annotation.Password;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordValidation implements ConstraintValidator<Password, char[]> {
+
+    @Override
+    public boolean isValid(char[] value, ConstraintValidatorContext context) {
+        if (value == null || value.length < 10) {
+            return false;
+        }
+
+        boolean isContainDigit = false;
+
+        for (char c : value) {
+            if (Character.isDigit(c)) {
+                isContainDigit = true;
+            }
+        }
+        return isContainDigit;
+    }
+}
